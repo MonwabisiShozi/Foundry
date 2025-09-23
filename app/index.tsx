24e0b91ba0b4796280c64d081f49437e6aaef728
Monwabisi <mtkshozi1@gmail.com>
2025-09-23 21:20:58 +0200

    Added a new set of fonts

--- app/index.tsx
+++ app/index.tsx
@@ -4,8 +4,8 @@ import { Text, View } from "react-native";
 export default function index() {
   return (
       <View className="flex-1 items-center justify-center bg-white">
-        <Text className="text-xl font-bold text-blue-500">
-          Welcome to Nativewind!
+        <Text className="text-5xl text-blue-500 font-Inter-Italic">
+          Welcome to NativeWind!
         </Text>
       </View>
   );
